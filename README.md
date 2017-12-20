1188cb193a32b6a33ae4080d7fcd4de6abedfe83
Woz4tetra <woz4tetra@gmail.com>
2017-12-20 15:23:18 -0500

    Backing up important files on naboris

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NaborisConfig
+Repository containing important configuration files on the raspberry pis for this project
---
+++ update-naboris.sh
@@ -0,0 +1,9 @@
+mkdir -p ./naboris/etc/network
+sudo cp /etc/network/interfaces ./naboris/etc/network/interfaces
+
+sudo cp /etc/dnsmasq.conf ./naboris/etc
+
+mkdir -p ./naboris/etc/hostapd/hostapd.conf
+sudo cp /etc/dnsmasq.conf ./naboris/etc/hostapd/dnsmasq.conf
+
+sudo cp /etc/rc.local ./naboris/etc
